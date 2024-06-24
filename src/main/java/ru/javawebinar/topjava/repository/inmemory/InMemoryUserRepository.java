f60ce94bdfbcc78fdce892041d651734026bd43a
Yuri Ignatev <yignatev@custis.ru>
2024-06-24 15:10:04 +0300

    hw2 реализовал InMemoryUserRepository все кроме getByEmail

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -5,36 +5,50 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.UsersUtil;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
-    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
-    @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return true;
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        UsersUtil.users.forEach(this::save);
     }
 
     @Override
     public User save(User user) {
+        if(user.isNew()){
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
         log.info("save {}", user);
-        return user;
+        return repository.computeIfPresent(user.getId(), (id, oldUser) ->user);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return repository.remove(id) != null;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return (List<User>) repository.values();
     }
 
     @Override
@@ -42,4 +56,5 @@ public class InMemoryUserRepository implements UserRepository {
         log.info("getByEmail {}", email);
         return null;
     }
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 }
