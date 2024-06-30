d3fe19fee540a2ed25e91f7733a67b41d5eb6702
Yuri Ignatev <yignatev@custis.ru>
2024-06-30 14:06:55 +0300

    hw2 3.3 возвращаю еду  в отсортированном в обратном прядке по дате

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,9 +5,11 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
@@ -40,7 +42,8 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Collection<Meal> getAll() {
-        return repository.values();
+        Comparator<Meal> compareByDate = Comparator.comparing(Meal::getDate).thenComparing(Meal::getDate);
+        return repository.values().stream().sorted(compareByDate.reversed()).collect(Collectors.toList());
     }
 }
 
