64dbd2aae9f4d2e2e16089a7affbe1c717c69041
Yuri Ignatev <yignatev@custis.ru>
2024-07-02 22:38:33 +0300

    hw2 User вернул к изначальному

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,79 +1,21 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Objects;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
-    private UserRepository repository;
-
-    public void init() {
-        repository = new InMemoryUserRepository();
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.setCharacterEncoding("UTF-8");
-        String id = request.getParameter("id");
-
-        User user = new User(id.isEmpty() ? null : Integer.valueOf(id),
-                request.getParameter("name"),
-                request.getParameter("email"),
-                request.getParameter("password"));
-
-        log.info(user.isNew() ? "Create {}" : "Update {}", user);
-        repository.save(user);
-        response.sendRedirect("users");
-    }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-
-        switch (action == null ? "all" : action) {
-            case "delete":
-                int id = getId(request);
-                log.info("Delete id={}", id);
-                repository.delete(id);
-                response.sendRedirect("users");
-                break;
-            case "create":
-            case "update":
-                final User user = "create".equals(action) ?
-                        new User(null, "", "", "", Role.USER) :
-                        repository.get(getId(request));
-                request.setAttribute("user", user);
-                request.getRequestDispatcher("/userForm.jsp").forward(request, response);
-                break;
-
-            case "all":
-            default:
-                log.info("getAll");
-                request.setAttribute("users", repository.getAll());
-                //              UsersUtil.getAll());
-                request.getRequestDispatcher("/users.jsp").forward(request, response);
-                break;
-
-        }
         log.debug("forward to users");
-
-    }
-
-    private int getId(HttpServletRequest request) {
-        String paramId = Objects.requireNonNull(request.getParameter("id"));
-        return Integer.parseInt(paramId);
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
