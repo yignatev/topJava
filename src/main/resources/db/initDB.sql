d29942b3390085a1384754d461c4d333ed92ae40
Yuri Ignatev <yignatev@custis.ru>
2024-07-06 07:57:10 +0300

    HW 3: инициализировал BD meals Сделал index

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP TABLE IF EXISTS meals;
+
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
@@ -23,3 +25,18 @@ CREATE TABLE user_role
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    dateTime    timestamp NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    userid      INTEGER   NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_date_time_and_userid ON meals (dateTime, userid);
+CREATE INDEX meals_calories on meals (calories);
+CREATE INDEX meals_userid on meals (userid);
+
+
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,10 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
+DELETE
+FROM meals;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +15,22 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (dateTime, description, calories, userid)
+values -- 100001  Admin Meals
+       ('2024-07-04 09:00:000', 'Amin_Завтрак', 500, 100001),
+       ('2024-07-04 12:00:000', 'Admin_Обед', 1000, 100001),
+       ('2024-07-04 18:00:000', 'Admin_Ужин', 500, 100001),
+       ('2024-07-04 18:30:000', 'Admin_Еда на граничное значение', 100, 100001),
+       ((now() - interval '1 day, 1 hour'), 'Admin_Завтрак', 1000, 100001),
+       ((now() - interval '1 day'), 'Admin_Обед', 500, 100001),
+       (((now() - interval '1 day') + interval '3 hours'), 'Admin_Ужин', 410, 100001),
+       -- 100000  USER Meals
+       ('2024-07-04 09:00:000', 'User_Завтрак', 500, 100000),
+       ('2024-07-04 12:00:000', 'User_Обед', 1000, 100000),
+       ('2024-07-04 18:00:000', 'User_Ужин', 500, 100000),
+       ('2024-07-04 18:30:000', 'User_Еда на граничное значение', 100, 100000),
+       ((now() - interval '1 day, 1 hour'), 'User_Завтрак', 1000, 100000),
+       ((now() - interval '1 day'), 'User_Обед', 500, 100000),
+       (((now() - interval '1 day') + interval '3 hours'), 'User_Ужин', 410, 100000);
+
