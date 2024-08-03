8c0011775c1a828c72568d9c9441b0d0227d89ea
Yuri Ignatev <yignatev@custis.ru>
2024-08-03 21:53:20 +0300

    HW_5 Meal тесты работают

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,21 +1,35 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
+@Transactional
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND" +
             " m.dateTime < :endDateTime ORDER BY m.dateTime DESC")
-    List<Meal> getBetweenHalfOpen (@Param("userId")Integer userId, @Param("startDateTime") LocalDateTime startDateTime,
-                                   @Param("endDateTime") LocalDateTime endDateTime);
+    List<Meal> getBetweenHalfOpen(@Param("userId") int userId, @Param("startDateTime") LocalDateTime startDateTime,
+                                  @Param("endDateTime") LocalDateTime endDateTime);
 
-    Meal getMealByUser (Integer mealId, Integer userId);
+    Meal getMealByUserIdAndId(int userId, int mealId);
 
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
-    List<Meal> getAllByUser (@Param("userId") Integer userId);
-    boolean deleteMealByUser(Integer mealId, Integer userId);
+    List<Meal> getAllByUserId(@Param("userId") int userId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m where m.id = :id AND m.user.id=:userId")
+    int deleteByIdAndByUserId(@Param("id") int id, @Param("userId") int userId);
+
+    @Query("SELECT m FROM Meal m where m.id = :id AND m.user.id=:userId")
+    Optional<Integer> findByIdAndByUserId(@Param("id") int id, @Param("userId") int userId);
+
+
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,8 +1,11 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,29 +14,45 @@ import java.util.List;
 public class DataJpaMealRepository implements MealRepository {
 
     private final CrudMealRepository crudRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    public DataJpaMealRepository(CrudMealRepository crudRepository,
+                                 CrudUserRepository crudUserRepository) {
         this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
+    @Transactional
+    @Modifying
     public Meal save(Meal meal, int userId) {
-        return crudRepository.save(meal);
+        if (meal.isNew()) {
+            meal.setUser(crudUserRepository.getReferenceById(userId));
+            return crudRepository.save(meal);
+        } else if (crudRepository.findByIdAndByUserId(meal.getId(), userId).isEmpty()) {
+            throw new NotFoundException("Not found entity with id=" + meal.getId());
+        } else {
+            meal.setCalories(meal.getCalories());
+            meal.setDescription(meal.getDescription());
+            meal.setDateTime(meal.getDateTime());
+            meal.setUser(crudUserRepository.getReferenceById(userId));
+            return crudRepository.save(meal);
+        }
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return crudRepository.deleteMealByUser(id, userId);
+        return crudRepository.deleteByIdAndByUserId(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return crudRepository.getMealByUser(id, userId);
+        return crudRepository.getMealByUserIdAndId(userId, id);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudRepository.getAllByUser(userId);
+        return crudRepository.getAllByUserId(userId);
     }
 
     @Override
